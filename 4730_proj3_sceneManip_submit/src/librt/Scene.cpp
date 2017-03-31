100e52a4e1124a0680c28df1d1a7e50273a4f108
Khari Ollivierre <ollivierre.khari@gmail.com>
2017-03-31 19:54:33 -0400

    Overloaded PropagateTransforms for TransformNodes

--- 4730_proj3_sceneManip_submit/src/librt/Scene.cpp
+++ 4730_proj3_sceneManip_submit/src/librt/Scene.cpp
@@ -657,12 +657,24 @@ void Scene::PropogateTransforms(SceneNode *pNode)
     for(int i = 0; i < (int)m_geometryList.size(); ++i) {
         STMatrix4 *worldmatrix = m_geometryList[i]->GetWorldT();
         if(IsTranslation(motion))
-            worldmatrix->Multiply(pNode->GetTransform());
+            worldmatrix->Multiply(m_trans);
         else if(IsRotation(motion))
             worldmatrix->Multiply(m_rotation);
     }
     //---------------------------------------------------------------------------------
 
+}
+
+void Scene::PropogateTransforms(TransformNode *pNode)
+{
+    ManipMotionType motion = CurrentManipMotion();
+    for(int i = 0; i < (int)m_geometryList.size(); ++i) {
+        STMatrix4 *worldmatrix = m_geometryList[i]->GetWorldT();
+        if(IsTranslation(motion))
+            worldmatrix->Multiply(pNode->GetTransform());
+        else if(IsRotation(motion))
+            worldmatrix->Multiply(m_rotation);
+    }
 }
 
 
